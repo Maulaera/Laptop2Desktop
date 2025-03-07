4b2ad08994873c2ba93a2f40cd7ca16aaba81b5b
Maulaera <mrobi021@odu.edu>
2025-03-06 21:03:21 -0500

    Task 3

--- HW2/main.py
+++ HW2/main.py
@@ -4,9 +4,12 @@
 import pandas as pd
 
 # Task One: Load data file into Pandas Data frame called "Gold"
-
 gold = pd.read_csv("daily_gold_rate.csv")
 
 # Task Two: List Data Types of df_gold and the first 10 rows/items
 print("Data Types:\n " , gold.dtypes)
 print("\nGold Data Frame:\n", gold.iloc[0:10])
+
+# Task Three: Conver to datetime format and list rows 100-150
+gold['Date'] = pd.to_datetime(gold['Date'])
+print("\nRows 100-150:\n" , gold.iloc[100:151])
